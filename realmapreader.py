e6d324c36a62a726207c24f4880fa6b1701dec7e
Nicolás Andrés Parada Mazitelli <nmazitelli@gmail.com>
2013-06-27 17:54:34 -0300

    update beta
    check it out, voy a ver si puedo checkear el command

--- realmapreader.py
+++ realmapreader.py
@@ -5,7 +5,7 @@ RIGHT, UP, LEFT, DOWN = (1,1), (1,-1), (-1,-1), (-1,1)
 NE, SE, NO, SO = (0,-1), (1,0), (-1,0), (0,1)
 
 FACINGS = {RIGHT:0, DOWN:1, NE:6, NO:3 ,UP:4 ,SE:5 ,SO:2 ,LEFT:7, (0,0): 0}
- 
+
 Key2Dir   = {275: RIGHT, 273:DOWN, 276:LEFT, 274:UP, K_ESCAPE:QUIT}
 NONE = (0,0)
 FPS = 45
@@ -20,53 +20,53 @@ HTILEW = 0.5 * TILEW
 def main():
 
     global FPSCLOCK, DISPLAYSURF, BASICFONT, aSpriteSheet, unMapa, aCamera, tileList, Commands
-        
+
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption('mapReader 2.0')
-    
+
     unMapa = readMap('pancamera.txt')
-    
-    
-    
+
+
+
     aSpriteSheet = spritesheet(unMapa.tileset)
     bSpriteSheet = spritesheet('swordwalking.png')
     aChar = character()
-    
+
     for directions in range(8):
         aChar.animations.append(bSpriteSheet.load_strip((0,directions*48,48,48), 8, colorkey=(0, 0, 0)))
-        
+
     tileList = {}
-    
-    command = (0,0)   
+
+    command = (0,0)
     Commands = []
-    aCamera = Camera('DYNAMIC', aChar)
-    
+    aCamera = Camera('STATIC', aChar)
+
     Collisionables = unMapa.getCollisionables()
-    
+
     while command != QUIT:
-        
-        #aCamera.update(aChar)
-        
-        #unMapa.draw(aCamera, aChar)
-        
-        #if aChar.moving:                
-        #    aChar.draw()
-        
+
+        aCamera.update(aChar)
+
+        unMapa.draw(aCamera, aChar)
+
+        if aChar.moving:
+            aChar.draw()
+
         command = getCommand()
-        
-        #aChar.update(command, Collisionables)
+
+        aChar.update(command, Collisionables)
         print command
         pygame.display.update()
         FPSCLOCK.tick(FPS)
-        
+
 def getCommand ():
         event = pygame.event.poll()
-        
+
         if event.type == pygame.KEYUP:
             Commands.remove(Key2Dir[event.key])
-        elif event.type == pygame.KEYDOWN:   
+        elif event.type == pygame.KEYDOWN:
             Commands.append(Key2Dir[event.key])
 
         if len(Commands) == 0:
@@ -75,20 +75,20 @@ def getCommand ():
             Acum = (0,0)
             for Command in Commands:
                 Acum = Acum[0] + Command[0], Acum[1] + Command[1]
-             
+
             if abs(Acum[0]) > 1:
                 if Acum[0] == 2:
                     Acum = 1 , Acum[1]
                 else:
                     Acum = -1 , Acum[1]
-                    
+
             if abs(Acum[1]) > 1:
                 if Acum[1] == 2:
                     Acum = Acum[0] , 1
                 else:
                     Acum = Acum[0] , -1
             return Acum
-        
+
 
 class Camera(object):
     x = 0
@@ -97,21 +97,21 @@ class Camera(object):
     mode = ''
     xRange = 0
     yRange = 0
-    
+
     def __init__(self, aMode, aChar):
         self.x = 0
         self.y = 0
         self.xyTile = 0,0
         self.mode = aMode
-        
+
         if self.mode == 'DYNAMIC':
             self.xyTile = copy.copy(aChar.pos)
             self.xRange, self.yRange = 10,10
         else:
             self.xRange, self.yRange = 5,5
             self.xyTile = aChar.pos
-            
-        
+
+
     def update(self, aChar):
         if self.mode == 'DYNAMIC':
             if aChar.pos[0] > (self.xyTile[0] + (self.xRange - 2)):
@@ -124,12 +124,12 @@ class Camera(object):
                 self.xyTile[1] -= 1
             else:
                 pass
-            
+
         self.x = HALF + (self.xyTile[0]*HTILEW) - (self.xyTile[1]*HTILEW)
         self.y =(self.xyTile[0] + self.xyTile[1]) * HTILEH + 32
         #tuple(map(int,getVertsOfTile(aChar.pos[0], aChar.pos[1])))
         self.x, self.y = (self.x - WINDOWWIDTH/2), (self.y - WINDOWHEIGHT/2)
-        
+
 class spritesheet(object):
     def __init__(self, filename):
         try:
@@ -150,7 +150,7 @@ class spritesheet(object):
         return image, rect
     # Load a whole bunch of images and return them as a list
     def images_at(self, rects, colorkey = None):
-        "Loads multiple images, supply a list of coordinates" 
+        "Loads multiple images, supply a list of coordinates"
         return [self.image_at(rect, colorkey) for rect in rects]
     # Load a whole strip of images
     def load_strip(self, rect, image_count, colorkey = None):
@@ -166,23 +166,23 @@ def drawtile(tile, Col, Fila):
     DISPLAYSURF.blit(tile, (X,Y))
 
 def readMap(filename):
-    assert os.path.exists(filename), 'No se puede encontrar el archivo' 
+    assert os.path.exists(filename), 'No se puede encontrar el archivo'
     mapFile = open(filename, 'r')
     # Cada nivel finaliza con una nueva linea
     lineas = mapFile.readlines() + ['\r\n']
     mapFile.close() # Cerramos el Archivo
 
     mapObj = mapObject() # Objeto mapa
-    
+
     readingLayer, readingData = False, False
     nLayers = -1
     nRows = -1
     csv = []
-    
+
     for nLinea in lineas:
         # Process each line that was in the level file.
         line = nLinea.rstrip('\r\n')
-        
+
         if ';' in line:
             # Ignore the ; lines, they're comments in the level file.
             line = line[:line.find(';')]
@@ -201,11 +201,11 @@ def readMap(filename):
             mapObj.tilesetYOffset = int(tilesetdata[4])
             mapObj.tilesetWidth = int(tilesetdata[5])
             mapObj.tilesetHeight = int(tilesetdata[6])
-            
+
         if '[layer]' in line:
             nLayers = nLayers + 1
             readingLayer = True
-            
+
         if readingLayer and line == '':
             readingLayer = False
             readingData = False
@@ -214,20 +214,20 @@ def readMap(filename):
             mapObj.layers.append((line[line.find('type='):].lstrip('type='),[]))
             nRows = 0
             #mapObj.layers[nLayers][0] = line[line.find('type='):].lstrip('type=')
-        
+
         if 'data=' in line:
             readingData = True
-        
+
         if readingLayer and readingData and 'data=' not in line:
             mapObj.layers[nLayers][1].append([])
             csv = line.rstrip(',').split(',')
             for id in csv:
-                trueId = int(id)  
+                trueId = int(id)
                 x = ((trueId-1) % (mapObj.tilesetWidth / mapObj.tilew)) * mapObj.tilew
                 y = int((trueId-1) / (mapObj.tilesetWidth / mapObj.tilew)) * mapObj.tileh
                 mapObj.layers[nLayers][1][nRows].append((x,y))
             nRows += 1
-                
+
     return mapObj
 
 class mapObject:
@@ -241,7 +241,7 @@ class mapObject:
     tilesetWidth = 0
     tilesetHeight = 0
     layers = []
-    
+
     def getCollisionables(self):
         Collisionables = []
         for y, row in enumerate(self.layers[2][1]):
@@ -268,89 +268,26 @@ class character:
     pos = [5,5]
     state = 0
     moving = False
-    
+
     def draw(self):
         drawtile(self.animations[FACINGS[self.facing]][self.state][0], self.pos[0], self.pos[1])
-        
-    def update(self, command, collisionables):
-        if not self.moving and command[0] in [UP, DOWN, LEFT, RIGHT] and command[1] == KEYDOWN:
-            #self.state = (self.state + 1) % len(self.animations[self.state])
-            #self.pos[0] += command[0][0] * 0.1
-            #self.pos[1] += command[0][1] * 0.1 
-            self.facing = command[0]
+    def update(self,command,collisionables):
+        if command == None:
+            self.moving = False
+            self.state = 0
+        xVec, yVec = command
+        self.facing = command
+        if not self.willCollision(collisionables):
             self.moving = True
-            
-        elif self.moving and command[0] in [UP, DOWN, LEFT, RIGHT] and command[1] == KEYDOWN:
-            if (command[0][0] + self.facing[0] + command[0][1] + self.facing[1]) != 0 and self.facing[1]+self.facing[0] != 1:
-                #self.state = (self.state + 1) % len(self.animations[self.state])
-
-                if (command[0][0] + self.facing[0]) > 1:
-                    xVec = 1
-                elif (command[0][0] + self.facing[0]) < -1:
-                    xVec = -1
-                else:
-                    xVec = command[0][0] + self.facing[0]
-                                
-                if (command[0][1] + self.facing[1]) > 1:
-                    yVec = 1
-                elif (command[0][1] + self.facing[1]) < -1:
-                    yVec = -1
-                else:
-                    yVec = command[0][1] + self.facing[1]
-                
-                #self.pos[0] += xVec * 0.1
-                #self.pos[1] += yVec * 0.1
-                self.facing = (xVec, yVec)
-                
-            else: pass
-                #self.state = (self.state + 1) % len(self.animations[self.state])
-                #self.pos[0] += self.facing[0] * 0.1
-                #self.pos[1] += self.facing[1] * 0.1 
-
-        elif self.moving and command[0] in [UP, DOWN, LEFT, RIGHT] and command[1] == KEYUP and self.facing not in [UP, DOWN, LEFT, RIGHT]:
-            if abs(self.facing[0] + self.facing[1]) == 1:
-                if self.facing[0] == 1:
-                    xVec = 2
-                    yVec = self.facing[1]
-                elif self.facing[1] == 1:
-                    yVec = 2
-                    xVec = self.facing[0]
-
-                if self.facing[0] == -1:
-                    xVec = -2
-                    yVec = self.facing[1]
-                elif self.facing[1] == -1:
-                    yVec = -2
-                    xVec = self.facing[0]
-            else:
-                xVec = self.facing[0]
-                yVec = self.facing[1]
-            
-            if (abs(xVec - command[0][0]) < 3) and (abs(yVec - command[0][1]) < 3):
-                self.facing = xVec - command[0][0], yVec - command[0][1]
-            #self.pos[0] += self.facing[0] * 0.1
-            #self.pos[1] += self.facing[1] * 0.1
-            
-
-        elif self.moving and command[0] in [UP, DOWN, LEFT, RIGHT] and command[1] == KEYUP and self.facing in [UP, DOWN, LEFT, RIGHT]:
-            if command[0] == self.facing:
-                self.moving = False
-                
-            
-        else:
-            pass
-            
-        if self.moving and not self.willCollision(collisionables):
-            self.pos[0] += self.facing[0] * 0.1
-            self.pos[1] -= self.facing[1] * 0.1
-
             self.state = (self.state + 1) % len(self.animations[FACINGS[self.facing]])
-        
+            X,Y = self.pos
+            self.pos = (X + xVec *0.1, Y-yVec * 0.1)
+
     def willCollision (self, collisionables):
         print ((self.pos[0] + (self.facing[0] * 0.1)),(self.pos[1] - (self.facing[1] * 0.1)))
         if (int(self.pos[0] + (self.facing[0] * 0.1))+1,int(self.pos[1] - (self.facing[1] * 0.1))+1) in collisionables:
             return True
         return False
-            
+
 if __name__ == '__main__':
     main()
