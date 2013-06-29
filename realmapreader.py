052f331d45a284757425ec7a2ff1e78665bda2e1
Nicolás Andrés Parada Mazitelli <nmazitelli@gmail.com>
2013-06-29 16:37:48 -0300

    Class LEVEL added
    Basically to manage levels, we can look forward to a bag menu now or
    stuff like that.

--- realmapreader.py
+++ realmapreader.py
@@ -5,7 +5,7 @@ RIGHT, UP, LEFT, DOWN = (1,1), (1,-1), (-1,-1), (-1,1)
 NE, SE, NO, SO = (0,-1), (1,0), (-1,0), (0,1)
 
 FACINGS = {RIGHT:0, DOWN:1, NE:6, NO:3 ,UP:4 ,SE:5 ,SO:2 ,LEFT:7, (0,0): 0}
- 
+
 Key2Dir   = {275: RIGHT, 273:DOWN, 276:LEFT, 274:UP, K_ESCAPE:QUIT}
 NONE = (0,0)
 FPS = 35
@@ -13,61 +13,109 @@ WINDOWWIDTH, WINDOWHEIGHT = 1000, 700
 TILEH, TILEW, GAPSIZE = 32, 64, 1
 BOARDWIDTH = 15
 
+#           R,    G,    B
+BLACK =     (0  , 0  ,  0  )
+WHITE =     (255, 255,  255)
+GREEN =     (0  , 160,  50 )
+
+
 def main():
 
     global FPSCLOCK, DISPLAYSURF, BASICFONT, aSpriteSheet, aRock, aGrass, aWater, Camera
-        
+
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 36)
     pygame.display.set_caption('mapReader 2.0')
-    
-    unMapa = readMap('pancamera.txt')
-    
-    aSpriteSheet = spritesheet(unMapa.tileset)
-    bSpriteSheet = spritesheet('swordwalking.png')
-    aChar = character()
-    
-    for directions in range(8):
-        aChar.animations.append(bSpriteSheet.load_strip((0,directions*48,48,48), 8, colorkey=(0, 0, 0)))
-        
-    tileList = {}
-    
-    Camera = 10, 10
+    mainWorld = level(True)
     command = NONE, NONE
-
+    Camera = 10, 10
     while command != QUIT:
-        DISPLAYSURF.fill((0,0,0))
-        print Camera
-        Camera = tuple(map(int,getVertsOfTile(aChar.pos[0], aChar.pos[1])))
-        Camera = (Camera[0]-WINDOWWIDTH/2), (Camera[1]-WINDOWHEIGHT/2)
-        for layer, tiles in unMapa.layers:
-            for pos, tile in enumerate(tiles):
-                posX, posY = pos % unMapa.width, pos // unMapa.width
-                if tile in tileList:
-                    drawtile(tileList[tile][0], posX, posY)
-                else:
-                    tileList[tile] =  aSpriteSheet.image_at((tile[0],tile[1],64,32), colorkey=(0, 0, 0))
-                    drawtile(tileList[tile][0], posX, posY)
-                    
-        command = getCommand()
+        mainWorld.update()
 
-        aChar.update(command)
-        aChar.draw() 
-          
 
-    
+
         pygame.display.update()
         FPSCLOCK.tick(FPS)
-        
+
 def getCommand ():
         event = pygame.event.poll()
-        
+
         if event.type == pygame.KEYUP:
             return Key2Dir[event.key] , KEYUP
-        elif event.type == pygame.KEYDOWN:   
+        elif event.type == pygame.KEYDOWN:
             return Key2Dir[event.key] , KEYDOWN
         return NONE, NONE
+
+def makeText(text, color, bgcolor, top, left):
+    # create the Surface and Rect objects for some text.
+    textSurf = BASICFONT.render(text, True, color, bgcolor)
+    textRect = textSurf.get_rect()
+    textRect.topleft = (top, left)
+    return (textSurf, textRect)
+
+class level():
+    room = 'start'
+    playable = False
+    background = BLACK
+
+    def __init__(self,playable):
+        self.playable = playable
+
+    def start(self):
+        DISPLAYSURF.fill(self.background)
+        START_SURF, START_RECT = makeText('New Game', WHITE, GREEN, WINDOWWIDTH*0.3, WINDOWHEIGHT*0.7)
+        EXIT_SURF, EXIT_RECT = makeText('Exit', WHITE, GREEN, WINDOWWIDTH*0.6, WINDOWHEIGHT*0.7)
+        DISPLAYSURF.blit(START_SURF, START_RECT)
+        DISPLAYSURF.blit(EXIT_SURF, EXIT_RECT)
+        for event in pygame.event.get(): # event handling loop
+            if event.type == MOUSEBUTTONUP:
+                if START_RECT.collidepoint(event.pos):
+                    self.room = 'world'
+                    self.playable = True
+                if EXIT_RECT.collidepoint(event.pos):
+                    pygame.exit()
+                    sys.quit()
+
+    def world(self):
+
+        unMapa = readMap('pancamera.txt')
+
+        aSpriteSheet = spritesheet(unMapa.tileset)
+        bSpriteSheet = spritesheet('swordwalking.png')
+        aChar = character()
+
+        for directions in range(8):
+            aChar.animations.append(bSpriteSheet.load_strip((0,directions*48,48,48), 8, colorkey=(0, 0, 0)))
+
+        tileList = {}
+
+        Camera = 10, 10
+        command = NONE, NONE
+        DISPLAYSURF.fill(self.background)
+        while True:
+            Camera = tuple(map(int,getVertsOfTile(aChar.pos[0], aChar.pos[1])))
+            Camera = (Camera[0]-WINDOWWIDTH/2), (Camera[1]-WINDOWHEIGHT/2)
+            for layer, tiles in unMapa.layers:
+                for pos, tile in enumerate(tiles):
+                    posX, posY = pos % unMapa.width, pos // unMapa.width
+                    if tile in tileList:
+                        drawtile(tileList[tile][0], posX, posY)
+                    else:
+                        tileList[tile] =  aSpriteSheet.image_at((tile[0],tile[1],64,32), colorkey=(0, 0, 0))
+                        drawtile(tileList[tile][0], posX, posY)
+
+                command = getCommand()
+
+                aChar.update(command)
+                aChar.draw()
+
+    def update(self):
+        if self.room == 'start':
+            self.start()
+        if self.room == 'world':
+            self.world()
 
 class spritesheet(object):
     def __init__(self, filename):
@@ -89,7 +137,7 @@ class spritesheet(object):
         return image, rect
     # Load a whole bunch of images and return them as a list
     def images_at(self, rects, colorkey = None):
-        "Loads multiple images, supply a list of coordinates" 
+        "Loads multiple images, supply a list of coordinates"
         return [self.image_at(rect, colorkey) for rect in rects]
     # Load a whole strip of images
     def load_strip(self, rect, image_count, colorkey = None):
@@ -100,8 +148,8 @@ class spritesheet(object):
 
 
 
-def getVertsOfTile(Cola, Fila): 
-    half = BOARDWIDTH*TILEW/2  
+def getVertsOfTile(Cola, Fila):
+    half = BOARDWIDTH*TILEW/2
     Y = -32 + (Cola + Fila) * 0.5 * TILEH + 64
     X = half + (Cola*(0.5*TILEW)) - (Fila*(0.5*TILEW))
     return X,Y
@@ -116,22 +164,22 @@ def drawtile(tile, Col, Fila):
 
 
 def readMap(filename):
-    assert os.path.exists(filename), 'No se puede encontrar el archivo' 
+    assert os.path.exists(filename), 'No se puede encontrar el archivo'
     mapFile = open(filename, 'r')
     # Cada nivel finaliza con una nueva linea
     lineas = mapFile.readlines() + ['\r\n']
     mapFile.close() # Cerramos el Archivo
 
     mapObj = mapObject() # Objeto mapa
-    
+
     readingLayer, readingData = False, False
     nLayers = -1
     csv = []
-    
+
     for nLinea in lineas:
         # Process each line that was in the level file.
         line = nLinea.rstrip('\r\n')
-        
+
         if ';' in line:
             # Ignore the ; lines, they're comments in the level file.
             line = line[:line.find(';')]
@@ -150,11 +198,11 @@ def readMap(filename):
             mapObj.tilesetYOffset = int(tilesetdata[4])
             mapObj.tilesetWidth = int(tilesetdata[5])
             mapObj.tilesetHeight = int(tilesetdata[6])
-            
+
         if '[layer]' in line:
             nLayers = nLayers + 1
             readingLayer = True
-            
+
         if readingLayer and line == '':
             readingLayer = False
             readingData = False
@@ -162,18 +210,18 @@ def readMap(filename):
         if 'type=' in line and readingLayer:
             mapObj.layers.append((line[line.find('type='):].lstrip('type='),[]))
             #mapObj.layers[nLayers][0] = line[line.find('type='):].lstrip('type=')
-        
+
         if 'data=' in line:
             readingData = True
-        
+
         if readingLayer and readingData and 'data=' not in line:
             csv = line.rstrip(',').split(',')
             for id in csv:
-                trueId = int(id)  
+                trueId = int(id)
                 x = ((trueId-1) % (mapObj.tilesetWidth / mapObj.tilew)) * mapObj.tilew
                 y = int((trueId-1) / (mapObj.tilesetWidth / mapObj.tilew)) * mapObj.tileh
                 mapObj.layers[nLayers][1].append((x,y))
-                
+
     return mapObj
 
 class mapObject:
@@ -194,18 +242,18 @@ class character:
     pos = [5,5]
     state = 0
     moving = False
-    
+
     def draw(self):
         drawtile(self.animations[FACINGS[self.facing]][self.state][0], self.pos[0], self.pos[1])
-        
+
     def update(self, command):
         if not self.moving and command[0] in [UP, DOWN, LEFT, RIGHT] and command[1] == KEYDOWN:
             #self.state = (self.state + 1) % len(self.animations[self.state])
             #self.pos[0] += command[0][0] * 0.1
-            #self.pos[1] += command[0][1] * 0.1 
+            #self.pos[1] += command[0][1] * 0.1
             self.facing = command[0]
             self.moving = True
-            
+
         elif self.moving and command[0] in [UP, DOWN, LEFT, RIGHT] and command[1] == KEYDOWN:
             if (command[0][0] + self.facing[0] + command[0][1] + self.facing[1]) != 0 and self.facing[1]+self.facing[0] != 1:
                 #self.state = (self.state + 1) % len(self.animations[self.state])
@@ -216,22 +264,22 @@ class character:
                     xVec = -1
                 else:
                     xVec = command[0][0] + self.facing[0]
-                                
+
                 if (command[0][1] + self.facing[1]) > 1:
                     yVec = 1
                 elif (command[0][1] + self.facing[1]) < -1:
                     yVec = -1
                 else:
                     yVec = command[0][1] + self.facing[1]
-                
+
                 #self.pos[0] += xVec * 0.1
                 #self.pos[1] += yVec * 0.1
                 self.facing = (xVec, yVec)
-                
+
             else: pass
                 #self.state = (self.state + 1) % len(self.animations[self.state])
                 #self.pos[0] += self.facing[0] * 0.1
-                #self.pos[1] += self.facing[1] * 0.1 
+                #self.pos[1] += self.facing[1] * 0.1
 
         elif self.moving and command[0] in [UP, DOWN, LEFT, RIGHT] and command[1] == KEYUP and self.facing not in [UP, DOWN, LEFT, RIGHT]:
             if abs(self.facing[0] + self.facing[1]) == 1:
@@ -251,28 +299,28 @@ class character:
             else:
                 xVec = self.facing[0]
                 yVec = self.facing[1]
-            
+
             if (abs(xVec - command[0][0]) < 3) and (abs(yVec - command[0][1]) < 3):
                 self.facing = xVec - command[0][0], yVec - command[0][1]
             #self.pos[0] += self.facing[0] * 0.1
             #self.pos[1] += self.facing[1] * 0.1
-            
+
 
         elif self.moving and command[0] in [UP, DOWN, LEFT, RIGHT] and command[1] == KEYUP and self.facing in [UP, DOWN, LEFT, RIGHT]:
             if command[0] == self.facing:
                 self.moving = False
-                
-            
+
+
         else:
             pass
-            
+
         if self.moving:
             self.pos[0] += self.facing[0] * 0.1
             self.pos[1] -= self.facing[1] * 0.1
 
             self.state = (self.state + 1) % len(self.animations[FACINGS[self.facing]])
-        
-    
+
+
 
 if __name__ == '__main__':
     main()
